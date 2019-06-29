448a3d13f92ae2dee6cdd9417079045880438c94
Arindam <abatabyal@gmail.com>
2019-06-29 22:20:18 +0530

    comments added

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,7 @@ from app.internal.UpdateBookAPI import UpdateBookAPI
 from app.external.ExternalAPI import ExternalAPI
 api = Api(app)
 
+#Flask REST API End points
 api.add_resource(ExternalAPI, '/api/external-books')
 api.add_resource(BookAPI, '/api/v1/books')
 api.add_resource(UpdateBookAPI, '/api/v1/books/<int:id>')
