5ae83c1367b5017d7538efa6c8fa3d39f949904c
Arindam <abatabyal@gmail.com>
2019-06-29 22:19:29 +0530

    comments added

--- app/tests/test_book_api.py
+++ app/tests/test_book_api.py
@@ -3,12 +3,16 @@ from unittest import TestCase
 from app import app
 
 class TestInternal(TestCase):
+
     def setUp(self):
        self.app = app
        self.client = self.app.test_client()
 
     #create book
     def test_create_positive(self):
+        """Positive unit test case for creating a book record
+        :return:
+        """
         response = self.client.post(path='/api/v1/books', data=json.dumps({
             "name": "My Test Book 33",
             "isbn": "123-3213243567",
@@ -25,6 +29,9 @@ class TestInternal(TestCase):
 
     # create book negative parameter missing
     def test_create_negative_missing_params(self):
+        """Negative unit test case for creating a book record with missing name
+        :return:
+        """
         response = self.client.post(path='/api/v1/books', data=json.dumps({
 
             "isbn": "123-3213243567",
@@ -39,8 +46,11 @@ class TestInternal(TestCase):
         }), content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
-    # create book negative parameter missing
+    # create book negative parameter empty
     def test_create_negative_empty_params(self):
+        """Negative unit test case for creating a book record with empty country
+        :return:
+        """
         response = self.client.post(path='/api/v1/books', data=json.dumps({
             "name": "My Test Book 33",
             "isbn": "123-3213243567",
@@ -55,30 +65,30 @@ class TestInternal(TestCase):
         }), content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
-    # get book list all
+    # positive testing get book list all
     def test_get_book_list(self):
         response = self.client.get(path='/api/v1/books')
         self.assertEqual(response.json['status_code'], 200)
 
-    # get book list by name
+    # positive testing get book list by name
     def test_get_book_name(self):
         response = self.client.get(path='/api/v1/books', data=json.dumps({"name": "My Test Book 33"}),
                                                              content_type='application/json')
         self.assertEqual(response.json['status_code'], 200)
 
-    # get book list by country
+    # positive testing get book list by country
     def test_get_book_country(self):
         response = self.client.get(path='/api/v1/books', data=json.dumps({"country": "United States"}),
                                    content_type='application/json')
         self.assertEqual(response.json['status_code'], 200)
 
-    # get book list by publisher
+    # positive testing get book list by publisher
     def test_get_book_publisher(self):
         response = self.client.get(path='/api/v1/books', data=json.dumps({"publisher": "Acme Books"}),
                                    content_type='application/json')
         self.assertEqual(response.json['status_code'], 200)
 
-    # get book list by year
+    # positive testing get book list by year
     def test_get_book_year(self):
         response = self.client.get(path='/api/v1/books', data=json.dumps({"year": "1999"}),
                                    content_type='application/json')
--- app/tests/test_external_api.py
+++ app/tests/test_external_api.py
@@ -6,12 +6,12 @@ class TestExternal(TestCase):
        self.app = app
        self.client = self.app.test_client()
 
-    #test with book name
+    # positive testing test with book name
     def test_ice_and_fire(self):
         response = self.client.get(path='/api/external-books?name=A Game of Thrones', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
-    # test without book name
+    # negativve testing test without book name
     def test_empty_book(self):
         response = self.client.get(path='/api/external-books?name=', follow_redirects=True)
         self.assertEqual(response.status_code, 400)
